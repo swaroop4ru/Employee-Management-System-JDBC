8ccb0bc16ecb180282df7350bb9e3cf848f51b37
swaroop4ru <penkejayaswaroop96@gmail.comn>
2026-08-15 18:55:16 +0530

    Complted Employee Management System

--- src/com/project/EMSPROJECT.java
+++ src/com/project/EMSPROJECT.java
@@ -36,11 +36,33 @@ public class EMSPROJECT {
 			 case 2:
 				 dao.viewAllEmployees();
 				 break;
+			 case 3:
+				 System.out.println("Enter id");
+				 int id = obj.nextInt();
+				 dao.searchEmployee(id);
+				 break;
+			 case 4:
+				 System.out.println("Enter id");
+				 id = obj.nextInt();
+				 System.out.println("Enter your salary");
+				 salary = obj.nextDouble();
+				 dao.updateSalary(id,salary);
+				 break;
+			 case 5:
+				 System.out.println("Enter id");
+				 id = obj.nextInt();
+				 System.out.println("Enter your Email");
+				  email = obj.next(); 
+				  dao.updateEmail(id, email);
+				  break;
+			 case 6:
+				 System.out.println("Enter id");
+				 id = obj.nextInt();
+				 dao.deleteEmployee(id);
+				 break;
 			 case 7 :
 				 System.out.println("TQS");
-				 return;
-				 
-				 
+				 return; 
 				 
 			 }
 			 
--- src/com/project/EmployeeDAO.java
+++ src/com/project/EmployeeDAO.java
@@ -9,6 +9,7 @@ public class EmployeeDAO {
 	
 	Connection connection = null;
 	
+	
 	public void addEmployee(String name, String email, String password, double salary) {
 		connection = ConnectionManager.getConnection();
 		String query ="insert into employee(name, email, password, salary)values(?,?,?,?)";
@@ -55,6 +56,104 @@ public class EmployeeDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+
+	public void searchEmployee(int id) {
+		Connection connection = ConnectionManager.getConnection();
+		try {
+			connection.setAutoCommit(true);
+			String query = "select *from employee WHERE id = ?";
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setInt(1, id);
+			ResultSet set = statement.executeQuery();
+			if(set.next()) {
+				System.out.print(set.getInt(1)+" ");
+				System.out.print(set.getString(2)+" ");
+				System.out.print(set.getString(3)+" ");
+				System.out.print(set.getString(4)+" ");
+				System.out.print(set.getDouble(5)+" ");
+				System.out.println();
+			}else {
+				System.out.println("Employee not found1!........");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void updateSalary(int id, double salary) {
+		connection = ConnectionManager.getConnection();
+		String query ="update Employee set salary = ? WHERE id = ?";
+		try {
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setDouble(1, salary);
+			statement.setInt(2, id);
+			
+			int count = statement.executeUpdate();
+			if(count != 0) {
+				connection.commit();
+				System.out.println("Record update  Successfully!....");
+			}else {
+				connection.rollback();
+				System.out.println("Error in  update adding employee details");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void updateEmail(int id, String email) {
+		connection = ConnectionManager.getConnection();
+		String query ="update Employee set email = ? WHERE id = ?";
+		try {
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, email);
+			statement.setInt(2, id);
+			
+			int count = statement.executeUpdate();
+			if(count != 0) {
+				connection.commit();
+				System.out.println("Record update  Successfully!....");
+			}else {
+				connection.rollback();
+				System.out.println("Error in  update adding employee details");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void deleteEmployee(int id) {
+		connection = ConnectionManager.getConnection();
+		String query ="delete from employee WHERE id = ?";
+		try {
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setInt(1, id);
+			
+			
+			int count = statement.executeUpdate();
+			if(count != 0) {
+				connection.commit();
+				System.out.println("Delete Employee Successfully!....");
+			}else {
+				connection.rollback();
+				System.out.println("Error in  deleteing   employee details");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
